6158d9e26116b8fca4661f72b7b1f97eaa0b6a6e
Kensany-patoupe-chenou <kensany.patoupechenou@edu.esiee.fr>
2025-09-19 09:31:00 +0000

    message explicatif

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
+"""
+Module pour vérifier si un nombre est premier et afficher les nombres premiers jusqu'à 100.
+"""
 from math import sqrt
-
 #### Fonction secondaire
 
-
 def isprime(p):
+    '''
+    Retourne le nombre premier p.
 
-    # votre code ici
+    Args:
+        p:nombre entierr positif
 
-    pass
-
+    returns:
+        bool: True si p est un nombre premier, False sinon.
+    '''
+    if p<2:
+        return False
+    for i in range(2,int(sqrt(p)) + 1):
+        if p%i==0:
+            return False
+    return True
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Fonction principale : teste et affiche les nombres premiers jusqu'à 100.
+    """
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
-
 if __name__ == "__main__":
     main()
